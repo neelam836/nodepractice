5657f157d744a6078f0e9ddb58c1f84811df1be6
neelam836 <neelam.joshi@gmail.com>
2025-07-23 20:59:40 -0700

    new edit

--- index.js
+++ index.js
@@ -1,43 +1,66 @@
-const exp = require('express');
+// const exp = require('express');
 
-const app = exp();
-app.use(exp.urlencoded({extended:true}))
+// const app = exp();
+// app.use(exp.urlencoded({extended:true}))
 
-app.use(exp.json()); 
-app.set("view engine","ejs")
+// app.use(exp.json()); 
+// app.set("view engine","ejs")
+// app.get('/', (req, res) => {
+//   // res.send('Hello World!');
+//   res.render("index",{"name":"neelam joshi"})
+// });
+
+// // app.get('/about', function (req, res)  {
+// //   res.send('About Page');
+// // });
+
+// // app.get('/about/:name',  function (req, res)  {
+// //   console.log("this is about");
+// //   res.send(`About page for ${req.params.name}`);
+// // });
+
+// // app.post("/product",  function(req, res)  {
+// //   console.log(req.body); 
+// //   // res.json({ name: "neelam" });
+// //   res.send(req.body);
+// //   res.end();
+// // });
+// // app.put("/product/:id",function (req,res) {
+
+// //   if(req.params.id==0)
+// //     res.status(500).json({message: "something went wrong"});
+// //   else
+// //   res.send("updated successfully");
+// // })
+// // app.delete('/delete/:id',function(req,res){
+// //   res.send("this is delete");
+// // })
+// const product= require("./routes/product");
+// app.use("/product",product);
+
+// app.listen(3000, () => {
+//   console.log("Server running at http://127.0.0.1:3000");
+// });
+
+const express = require('express');
+const app = express();
+const productRoutes = require('./routes/product'); // your router
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// ✅ Serve static assets (optional)
+app.use(express.static('public'));
+
+// ✅ Mount the router
+app.use('/product', productRoutes);
+
+// ✅ Redirect root "/" to "/product"
 app.get('/', (req, res) => {
-  // res.send('Hello World!');
-  res.render("index",{"name":"neelam joshi"})
+  res.redirect('/product');
 });
 
-// app.get('/about', function (req, res)  {
-//   res.send('About Page');
-// });
-
-// app.get('/about/:name',  function (req, res)  {
-//   console.log("this is about");
-//   res.send(`About page for ${req.params.name}`);
-// });
-
-// app.post("/product",  function(req, res)  {
-//   console.log(req.body); 
-//   // res.json({ name: "neelam" });
-//   res.send(req.body);
-//   res.end();
-// });
-// app.put("/product/:id",function (req,res) {
-
-//   if(req.params.id==0)
-//     res.status(500).json({message: "something went wrong"});
-//   else
-//   res.send("updated successfully");
-// })
-// app.delete('/delete/:id',function(req,res){
-//   res.send("this is delete");
-// })
-const product= require("./routes/product");
-app.use("/product",product);
-
 app.listen(3000, () => {
-  console.log("Server running at http://127.0.0.1:3000");
+  console.log('Server started at http://localhost:3000');
 });
--- routes/product.js
+++ routes/product.js
@@ -1,71 +1,134 @@
-const exp = require("express");
+// const exp = require("express");
+// const { MongoClient, ObjectId } = require("mongodb");
+// const uri = "mongodb://localhost:27017";
+
+// async function connects(collectionname = "students") {
+//   const client = new MongoClient(uri);
+//   await client.connect();
+//   console.log("connect to database");
+
+//   const db = client.db("mern");
+//   return db.collection(collectionname);
+// }
+
+// async function getrResult() {
+//   const result = (await connects()).find().toArray();
+//   return result;
+// }
+// const app = exp.Router();
+
+// app.get("/", (req, res) => {
+//   getrResult().then((data) => {
+//     // console.log(data);
+//     res.render("product/index", { data });
+//   });
+//   // res.send("this is product list");
+// });
+// app.post("/", async (req, res) => {
+//   (await connects()).insertOne(req.body);
+//   res.redirect("/product"); // or render a success page
+
+//   //    console.log(req.body);
+//   // let {name,price}=req.body;
+//   // res.render('product/show',req.body);
+//   // res.render(`Name:<b>${name}</b><br>Price:<b>${price}</b>`);
+
+//   // res.send("this is post  form product list");
+// });
+// app.get("/create", (req, res) => {
+//   // res.send("this is create  product list");
+//   res.render("product/create");
+// });
+// app.delete('/delete/:id', async (req, res) => {
+//   const db = client.db('mern');
+//   const collection = db.collection('students');
+//   await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+//   res.json({ message: 'Item deleted' });
+//    res.redirect("/product");
+// });
+
+// // app.post("/delete/:id", async (req, res) => {
+// // //   const collection = await connects();
+// //   (await connects()).deleteOne({ _id: new ObjectId(req.params.id) });
+// //   res.redirect("/product");
+// // });
+// // app.get("/:id",(req,res)=>{
+// //     res.send("this is find product list");
+
+// // });
+// // app.put("/:id",(req,res)=>{
+// //     res.send("this is updated  product list");
+
+// // });
+// // app.delete("/:id",(req,res)=>{
+// //     res.send("this is delete product list");
+
+// // });
+// app
+//   .route("/:id")
+//   .get((req, res) => {
+//     res.send("this is find list");
+//   })
+//   .put((req, res) => {
+//     res.send("this is put list");
+//   })
+//   .delete((req, res) => {
+//     res.send("this is delete list");
+//   });
+
+// module.exports = app;
+
+const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 
-async function connects(collectionname = "students") {
+const router = express.Router();
+
+// MongoDB connection helper
+async function connects(collectionName = "students") {
   const client = new MongoClient(uri);
   await client.connect();
-  console.log("connect to database");
-
+  console.log("Connected to database");
   const db = client.db("mern");
-  return db.collection(collectionname);
+  return db.collection(collectionName);
 }
 
-async function getrResult() {
-  const result = (await connects()).find().toArray();
-  return result;
-}
-const app = exp.Router();
-
-app.get("/", (req, res) => {
-  getrResult().then((data) => {
-    // console.log(data);
-    res.render("product/index", { data });
-  });
-  // res.send("this is product list");
+// Get all students
+router.get("/", async (req, res) => {
+  const data = await (await connects()).find().toArray();
+  res.render("product/index", { data });
 });
-app.post("/", async (req, res) => {
-  (await connects()).insertOne(req.body);
-  res.redirect("/product"); // or render a success page
 
-  //    console.log(req.body);
-  // let {name,price}=req.body;
-  // res.render('product/show',req.body);
-  // res.render(`Name:<b>${name}</b><br>Price:<b>${price}</b>`);
-
-  // res.send("this is post  form product list");
-});
-app.get("/create", (req, res) => {
-  // res.send("this is create  product list");
+// Render create form
+router.get("/create", (req, res) => {
   res.render("product/create");
 });
-app.post("/delete/:id", async (req, res) => {
-//   const collection = await connects();
-  (await connects()).deleteOne({ _id: new ObjectId(req.params.id) });
+
+// Add a new student
+router.post("/", async (req, res) => {
+  await (await connects()).insertOne(req.body);
   res.redirect("/product");
 });
-// app.get("/:id",(req,res)=>{
-//     res.send("this is find product list");
 
-// });
-// app.put("/:id",(req,res)=>{
-//     res.send("this is updated  product list");
+// DELETE using DELETE method (via fetch/axios)
+router.delete("/delete/:id", async (req, res) => {
+  await (await connects()).deleteOne({ _id: new ObjectId(req.params.id) });
+  res.json({ message: "Deleted successfully" });
+});
 
-// });
-// app.delete("/:id",(req,res)=>{
-//     res.send("this is delete product list");
+// PUT method (update by ID)
+router.put("/:id", async (req, res) => {
+  await (await connects()).updateOne(
+    { _id: new ObjectId(req.params.id) },
+    { $set: req.body }
+  );
+  res.json({ message: "Updated successfully" });
+});
 
-// });
-app
-  .route("/:id")
-  .get((req, res) => {
-    res.send("this is find list");
-  })
-  .put((req, res) => {
-    res.send("this is put list");
-  })
-  .delete((req, res) => {
-    res.send("this is delete list");
-  });
+// GET one student
+router.get("/:id", async (req, res) => {
+  const data = await (await connects()).findOne({ _id: new ObjectId(req.params.id) });
+  res.json(data);
+});
 
-module.exports = app;
+module.exports = router;
